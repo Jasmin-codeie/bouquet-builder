2c0afefff7324140add028af8b463f21ed1a30e2
Jasmin-codeie <jasmin.shabieva@gmail.com>
2020-03-28 16:57:30 +0600

    Created some components

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Toolbar from "./Toolbar/Toolbar";
+import SideDrawer from "./SideDrawer/SideDrawer";
+
+export default () => (
+    <div className="Layout"> 
+    <Toolbar />
+    <SideDrawer />
+    </div>
+);
