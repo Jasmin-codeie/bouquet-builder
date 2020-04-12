d9ad68357e2329544a11caf1a39dce0e2bf0c7ac
Jasmin-codeie <jasmin.shabieva@gmail.com>
2020-04-13 01:00:28 +0600

    Added component BouquetControls

--- src/containers/BouquetBuilder/BouquetBuilder.js
+++ src/containers/BouquetBuilder/BouquetBuilder.js
@@ -1,10 +1,56 @@
-import React from 'react';
-import classes from "./BouquetBuilder.module.css"
+import React, { useState } from "react";
 import Bouquet from "../../components/BouquetBuilder/Bouquet/Bouquet";
+import classes from "./BouquetBuilder.module.css";
+import SushiControls from "../../components/BouquetBuilder/BouquetControls/BouquetControls";
 
-export default () => (
+const PRICES = {
+    roses: 7,
+    tulips: 10,
+    daisies: 8,
+    lilies: 11,
+    irises: 15,
+    chrysanthemums: 12,
+};
+
+export default () => {
+  const [flowers, setFlowers] = useState({
+    roses: 0,
+    tulips: 0,
+    daisies: 0,
+    lilies: 0,
+    irises: 0,
+    chrysanthemums: 0,
+  });
+  const [price, setPrice] = useState(80);
+
+  function addFlowers(type) {
+    const newFlowers = { ...flowers };
+    newFlowers[type]++;
+    setFlowers(newFlowers);
+
+    const newPrice = price + PRICES[type];
+    setPrice(newPrice);
+  }
+
+  function removeFlower(type) {
+    if (flowers[type] >= 1) {
+      const newFlowers = { ...flowers };
+      newFlowers[type]--;
+      setFlowers(newFlowers);
+
+      const newPrice = price - PRICES[type];
+      setPrice(newPrice);
+    }
+  }
+
+  return (
     <div className={classes.BouquetBuilder}>
-        <Bouquet />
-        BouquetControls
+      <Bouquet price={price} flowers={flowers} />
+      <BouquetControls
+        flowers={flowers}
+        addFlowers={addFlowers}
+        removeFlowers={removeFlowers}
+      />
     </div>
-);
+  );
+};
