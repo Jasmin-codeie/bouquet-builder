ab8953e018d8aacfeb7b9594fd4218f898bc6400
Jasmin-codeie <jasmin.shabieva@gmail.com>
2020-04-13 01:40:33 +0600

    Added some styles and components

--- src/components/BouquetBuilder/Bouquet/Bouquet.js
+++ src/components/BouquetBuilder/Bouquet/Bouquet.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from "react";
+import Sushi from "./Flower/Flower";
 import classes from "./Bouquet.module.css";
-import Flower from "./Flower/Flower";
 
-export default () => (
+export default ({ price, flowers }) => {
+  let flowersOutput = [];
+
+  Object.keys(
+      flowers).forEach((type) => {
+    for (let i = 0; i < flowers[type]; i++) {
+      flowersOutput.push(<Flower key={type + i} type={type} />);
+    }
+  });
+
+  return (
     <div className={classes.Bouquet}>
-        <Flower />
+      <div className={classes.bento}>{flowersOutput}</div>
+      <div className={classes.price}>Total price: {price} som</div>
     </div>
-);
+  );
+};
