bac68460cf158209349e4797c0c104461f89eee1
Jasmin-codeie <jasmin.shabieva@gmail.com>
2020-04-13 01:11:13 +0600

    Added component Bouquet Control

---
+++ src/components/BouquetBuilder/BouquetControls/BouquetControl/BouquetControl.js
@@ -0,0 +1,21 @@
+import React from "react";
+import classes from "./BouquetControl.module.css";
+
+export default ({ control, removeFlowers, addFlowers, disabled }) => (
+  <div className={classes.BouquetControl}>
+    <button
+      className={classes.less}
+      onClick={() => removeFlowers(control.type)}
+      disabled={disabled}
+    >
+      -
+    </button>
+    <span className={classes.label}>{control.label}</span>
+    <button
+      className={classes.more}
+      onClick={() => addFlowers(control.type)}
+    >
+      +
+    </button>
+  </div>
+);
