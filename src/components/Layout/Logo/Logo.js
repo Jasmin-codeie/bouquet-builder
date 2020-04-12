daf25cfc7def968136ab8191c8354c363044c4ab
Jasmin-codeie <jasmin.shabieva@gmail.com>
2020-04-13 01:28:22 +0600

    Added logo-image

--- src/components/Layout/Logo/Logo.js
+++ src/components/Layout/Logo/Logo.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import classes from  './Logo.module.css';
+import React from "react";
+import logo from "../../../assets/logo.jpg";
+import classes from "./Logo.module.css";
 
-export default () =>(
-    <div className={classes.Logo}>
-
-    </div>
+export default () => (
+  <div className={classes.Logo}>
+    <img src={logo} />
+    <span>Bouquet Builder</span>
+  </div>
 );
