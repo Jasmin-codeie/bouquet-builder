9329eeda50d94be647db53e09654b69e8a785167
Jasmin-codeie <jasmin.shabieva@gmail.com>
2020-04-18 23:51:15 +0600

    Added images of flowers

--- src/components/BouquetBuilder/Bouquet/Flower/Flower.js
+++ src/components/BouquetBuilder/Bouquet/Flower/Flower.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import classes from "./Flower.module.css"
+import classes from "./Flower.module.css";
 
 export default ({ type }) => {
     const flowerClasses = [classes.Flower, classes[type]];
