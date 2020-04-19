e1dddb1b69412c5c93c4461528c211c6326f7b8f
Jasmin-codeie <jasmin.shabieva@gmail.com>
2020-04-19 22:59:52 +0600

    Added some changes

--- src/components/Layout/Drawer/Drawer.js
+++ src/components/Layout/Drawer/Drawer.js
@@ -12,7 +12,6 @@ export default ({ toggleDrawer, open }) => {
     <div className={classes.Drawer}>
       <Backdrop hideCallBack={toggleDrawer} show={open} />
       <div className={contentClasses.join(" ")}>
-        {" "}
         <Logo />
         <Navigation />
       </div>
