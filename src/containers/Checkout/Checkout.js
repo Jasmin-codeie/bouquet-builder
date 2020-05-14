5fbade056ec3d9c453c209722fd130c17c327255
Jasmin-codeie <jasmin.shabieva@gmail.com>
2020-05-14 21:21:12 +0600

    Added main part of CheckoutForm

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,30 +1,59 @@
-import React from "react";
-import classes from "./Checkout.module.css";
-import CheckoutSummary from "../../components/Checkout/CheckoutSummary/CheckoutSummary";
+import React, { useState, useEffect } from "react";
 import { useHistory, useLocation, Route } from "react-router-dom";
+import axios from "../../axios";
+import CheckoutSummary from "../../components/Checkout/CheckoutSummary/CheckoutSummary";
+import classes from "./Checkout.module.css";
+import CheckoutForm from "./CheckoutForm/CheckoutForm";
+import withErrorHandler from "../../hoc/withErrorHadler/withErrorHadler";
+import Spinner from "../../components/UI/Spinner/Spinner";
 
-export default () => {
+export default withErrorHandler(() => {
   const history = useHistory();
   const location = useLocation();
+  const [flowers, setFlowers] = useState({});
+  const [price, setPrice] = useState(0);
+  const [loading, setLoading] = useState(false);
 
-  const query = new URLSearchParams(location.search);
-  let price = 0;
-  let flowers = {};
-  query.forEach((value, key) => {
-    if (key === "price") {
-      price = +value;
-    } else {
-      flowers[key] = +value;
-    }
-  });
+  useEffect(() => {
+    const query = new URLSearchParams(location.search);
+    const newFlowers = {};
+    query.forEach((value, key) => {
+      if (key === "price") {
+        setPrice(+value);
+      } else {
+        newFlowers[key] = +value;
+      }
+    });
+    setFlowers(newFlowers);
+  }, []);
 
   function checkoutCancel() {
     history.push("/builder");
   }
 
   function checkoutContinue() {
-    history.push("/checkout/contacts");
+    history.push("/checkout/form");
   }
+
+  function checkoutFinish(data) {
+    setLoading(true);
+    axios
+      .post("/orders.json", {
+        flowers,
+        price,
+        details: data,
+      })
+      .then((response) => {
+        setLoading(false);
+        history.replace("/");
+      });
+  }
+
+  let formOutput = <Spinner />;
+  if (!loading) {
+    formOutput = <CheckoutForm checkoutFinish={checkoutFinish} />;
+  }
+
   return (
     <div className={classes.Checkout}>
       <CheckoutSummary
@@ -33,7 +62,7 @@ export default () => {
         checkoutCancel={checkoutCancel}
         checkoutContinue={checkoutContinue}
       />
-      <Route path="/checkout/contacts"></Route>
+      <Route path="/checkout/form">{formOutput}</Route>
     </div>
   );
-};
+}, axios);
