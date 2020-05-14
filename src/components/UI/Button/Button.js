ee381e838bd335779fbb1a12bb334ffcb1dccb6b
Jasmin-codeie <jasmin.shabieva@gmail.com>
2020-05-14 17:37:52 +0600

    Added colours to buttons

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,11 +1,16 @@
 import React from "react";
 import classes from "./Button.module.css";
 
-export default ({ children, click, enabled }) => {
+export default ({ children, click, enabled, red, green }) => {
+  const buttonClasses = [classes.Button];
+
+  if (red) buttonClasses.push(classes.red);
+  if (green) buttonClasses.push(classes.green);
+
   return (
     <button
       onClick={click}
-      className={classes.Button}
+      className={buttonClasses.join(" ")}
       disabled={enabled === undefined ? false : !enabled}
     >
       {children}
