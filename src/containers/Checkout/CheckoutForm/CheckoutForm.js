ef6ca766e0667c75f5be264572d112a35d5e4119
Jasmin-codeie <jasmin.shabieva@gmail.com>
2020-05-23 23:16:30 +0600

    Fixed checkout page

--- src/containers/Checkout/CheckoutForm/CheckoutForm.js
+++ src/containers/Checkout/CheckoutForm/CheckoutForm.js
@@ -1,74 +1,34 @@
-import React, { useState } from "react";
+import React from "react";
 import Button from "../../../components/UI/Button/Button";
 import classes from "./CheckoutForm.module.css";
 
 export default ({ checkoutFinish }) => {
-  const [data, setData] = useState({
-    name: "",
-    phone: "",
-    city: "",
-    address: "",
-    delivery: "",
-  });
+  function formSubmit({ event }) {
+    const data = new FormData(event.target);
+    checkoutFinish({
+      name: data.get("name"),
+      phone: data.get("phone"),
+      city: data.get("city"),
+      address: data.get("address"),
+      delivery: data.get("delivery"),
+    });
 
-  function nameChange({ target }) {
-    setData({ ...data, name: target.value });
-  }
-
-  function phoneChange({ target }) {
-    setData({ ...data, phone: target.value });
-  }
-
-  function addressChange({ target }) {
-    setData({ ...data, address: target.value });
-  }
-  function cityChange({ target }) {
-    setData({ ...data, city: target.value });
-  }
-
-  function deliveryChange({ target }) {
-    setData({ ...data, delivery: target.value });
+    event.preventDefault();
   }
 
   return (
-    <div className={classes.CheckoutForm}>
-      <input
-        type="text"
-        placeholder="Name"
-        onChange={nameChange}
-        value={data.name}
-        required
-      />
-      <input
-        type="phone"
-        placeholder="Phone"
-        onChange={phoneChange}
-        value={data.phone}
-        required
-      />
-      <input
-        type="text"
-        placeholder="City"
-        onChange={cityChange}
-        value={data.city}
-        required
-      />
-      <input
-        type="text"
-        placeholder="Address"
-        onChange={addressChange}
-        value={data.address}
-        required
-      />
-      <select onChange={deliveryChange} required>
+    <form onSubmit={formSubmit} className={classes.CheckoutForm}>
+      <input type="text" placeholder="Name" name="name" required />
+      <input type="phone" placeholder="Phone" name="phone" required />
+      <input type="text" placeholder="City" name="city" required />
+      <input type="text" placeholder="Address" name="address" required />
+      <select name="delivery" required>
         <option value="">- Delivery -</option>
         <option>Fastest</option>
         <option>Fast</option>
         <option>Regular</option>
       </select>
-      <Button click={() => checkoutFinish(data)} green>
-        Finish
-      </Button>
-    </div>
+      <Button green>Finish</Button>
+    </form>
   );
 };
