2693b624242c3a1ca77057f87c877f70c9c2b691
Jasmin-codeie <jasmin.shabieva@gmail.com>
2020-06-03 14:15:24 +0600

    Fixed the error in Order component

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,14 +1,6 @@
 import React from "react";
 import classes from "./Order.module.css";
 
-const CONTROLS = {
-  roses: "Roses",
-  jasmine: "Jasmine",
-  daisies: "Daisies",
-  lilies: "Lilies",
-  irises: "Irises",
-  liliesOfTheValley: "Lilies Of The Valley",
-};
 export default ({ price, flowers, details }) => {
   const flowersOutput = Object.keys(flowers)
     .filter((flower) => flowers[flower].quantity > 0)
