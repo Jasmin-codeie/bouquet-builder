cee0bc253d5c0173f7f6004e136973a0f96c501c
Jasmin-codeie <jasmin.shabieva@gmail.com>
2020-06-07 14:49:17 +0600

    Added auth logic to Nav

--- src/components/UI/Nav/Nav.js
+++ src/components/UI/Nav/Nav.js
@@ -1,11 +1,16 @@
 import React from "react";
 import NavItem from "./NavItem/NavItem";
 import classes from "./Nav.module.css";
+import { useSelector } from "react-redux";
 
-export default () => (
-  <ul className={classes.Nav}>
-    <NavItem url="/builder">Bouquet Builder</NavItem>
-    <NavItem url="/orders">Orders</NavItem>
-    <NavItem url="/autho">Authentication</NavItem>
-  </ul>
-);
+export default () => {
+  const isAuthenticated = useSelector((state) => state.autho.token != null);
+  return (
+    <ul className={classes.Nav}>
+      <NavItem url="/builder">Bouquet Builder</NavItem>
+      {isAuthenticated ? <NavItem url="/orders">Orders</NavItem> : null}
+      {!isAuthenticated ? <NavItem url="/autho">Authentication</NavItem> : null}
+      {isAuthenticated ? <NavItem url="/logout">Logout</NavItem> : null}
+    </ul>
+  );
+};
